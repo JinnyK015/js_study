62075a63da6a2ba0f610508a783ae89d92b96c0b
JinnyK015 <79676812+JinnyK015@users.noreply.github.com>
2021-07-09 22:13:43 +1200

    drawing color change Js
    I tried to use arrow function more then before.

--- app.js
+++ app.js
@@ -4,40 +4,43 @@
 let drawing = false;
 
 const canvas = document.getElementById("jsCanvas");
+const colors = document.querySelectorAll(".jsColor");
 const ctx = canvas.getContext("2d");
 
-
-ctx.strokeStyle= "#000000";
-ctx.lineWidth= 2.5;
-
-console.log(canvas.width, canvas.height);
-
+// we need to bring canvas's size( couldn't read from css)
 canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
 
+//basic color and weight of line (when you first painting)
+ctx.strokeStyle= "#000000";
+ctx.lineWidth= 2.5;
+
+canvas.addEventListener('mousemove', (e) => {
+    const x = e.offsetX;
+    const y = e.offsetY;
+    if(!drawing){
+        ctx.beginPath(); // call start creat new path 
+        ctx.moveTo(x,y); // start of coordinates좌표를따라 point create  = making path
+    } else {
+        ctx.lineTo(x,y); // 선 끝 좌표  end of line coordinates.
+        ctx.stroke(); // 선을 그린다.
+    }
+})
+
+canvas.addEventListener('mousedown', (e) => {
+    drawing = true;
+})
+
+canvas.addEventListener('mouseup', (e) =>{
+    drawing = false;
+})
+
+canvas.addEventListener('mouseleave', (e) => {
+    drawing = false;
+}) 
 
 
-    canvas.addEventListener('mousemove', (e) => {
-        const x = e.offsetX;
-        const y = e.offsetY;
-        if(!drawing){
-            ctx.beginPath(); // call start creat new path 
-            ctx.moveTo(x,y); // start of coordinates좌표를따라 point create  = making path
-        } else {
-            ctx.lineTo(x,y); // 선 끝 좌표  end of line coordinates.
-            ctx.stroke(); // 선을 그린다.
-        }
-    }),
-
-    canvas.addEventListener('mousedown', (e) => {
-        drawing = true;
-    }),
-
-    canvas.addEventListener('mouseup', (e) =>{
-        drawing = false;
-    }),
-
-    canvas.addEventListener('mouseleave', (e) => {
-        drawing = false;
-    })
-
+Array.from(colors).forEach(color => color.addEventListener('click', (e) =>{
+    const color = e.target.style.backgroundColor;
+    ctx.strokeStyle= color;
+}));
